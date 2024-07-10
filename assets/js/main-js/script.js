600f9aec551a4666fd13093a97090e95513f0c35
Subhadeep Samanta <subhadeep.99graphicdesigner@gmail.com>
2024-07-10 12:34:46 +0000

    noob5

--- assets/js/main-js/script.js
+++ assets/js/main-js/script.js
@@ -1,16 +1,18 @@
 $('.owl-carousel').owlCarousel({
     loop: true,
+    autoplay: true,
     margin: 10,
     nav: true,
+    dots: true,
     responsive: {
         0: {
             items: 1
         },
         600: {
-            items: 3
+            items: 2
         },
         1000: {
-            items: 5
+            items: 3
         }
     }
 })
